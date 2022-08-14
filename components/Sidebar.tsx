588eeea82511aad32eecb4cd7c2f53e86ed1394b
Henry Nguyen <henry.nguyen@lumi.com.au>
2022-08-14 22:29:16 +1000

    login logout working

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,14 +6,27 @@ import {
 	RssIcon,
 	SearchIcon
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 import IconButton from './IconButton'
 
 const Divider = () => <hr className='border-t-[0.1px] border-gray-900' />
 
 const Sidebar = () => {
+	const { data: session } = useSession()
+
 	return (
 		<div className='text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hidden sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block'>
 			<div className='space-y-4'>
+				{session?.user && (
+					<button
+						onClick={() => {
+							signOut()
+						}}
+					>
+						{session.user.name} - Log Out
+					</button>
+				)}
+
 				<IconButton icon={HomeIcon} label='Home' />
 				<IconButton icon={SearchIcon} label='Search' />
 				<IconButton icon={LibraryIcon} label='Your Library' />
